c0ade9a2b4ab1e2a0057f61583ab30fbe413340e
gamejamjam <gamejamjamjam@gmail.com>
2016-04-23 17:54:09 +0900

    sceneMng追加

---
+++ GameJamJamJam Project/Assets/users/sakai/sceneMng.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceneMng : MonoBehaviour {
+
+	public enum eSceneChangeType
+	{
+		ToGame,
+		ToResult,
+		ToTitle,
+	}
+
+	public eSceneChangeType mSceneChangeType;
+
+	const float cDelayTime = 0.4f;
+	float mDelayTimer;
+
+	// Use this for initialization
+	void Start () {
+		mDelayTimer = cDelayTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (mSceneChangeType) {
+		case eSceneChangeType.ToGame:
+			mDelayTimer -= Time.deltaTime;
+			if(  mDelayTimer < 0 && Input.anyKeyDown)
+			{
+				mDelayTimer = cDelayTime;
+				Application.LoadLevel ("MainGame");
+			}
+
+			break;
+		case eSceneChangeType.ToResult:
+			break;
+		case eSceneChangeType.ToTitle:
+			mDelayTimer -= Time.deltaTime;
+			if( mDelayTimer < 0 && Input.anyKeyDown)
+			{
+				mDelayTimer = cDelayTime;
+				Application.LoadLevel ("Title");
+			}
+			break;
+
+		}
+
+	}
+}
