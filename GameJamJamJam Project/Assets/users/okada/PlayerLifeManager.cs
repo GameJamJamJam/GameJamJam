cb44f71d6e5e0305aba5981108a427af9cc41f05
pafuhana1213 <pafuhana@hotmail.co.jp>
2016-04-23 14:23:00 +0900

    Player関係のスクリプトを幾つかコミット

---
+++ GameJamJamJam Project/Assets/users/okada/PlayerLifeManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLifeManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// プレイヤーにダメージを与える
+    /// </summary>
+    /// <param name="value"></param>
+    public void ApplayDamage( float value )
+    {
+
+
+    }
+}
