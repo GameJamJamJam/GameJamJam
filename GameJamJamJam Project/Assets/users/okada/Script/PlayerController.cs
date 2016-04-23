4016634b9a706a70e87579a98bf6b008c4e26183
pafuhana1213 <pafuhana@hotmail.co.jp>
2016-04-23 15:04:08 +0900

    コンポーネントが重複してたので、プレハブ修正

--- GameJamJamJam Project/Assets/users/okada/Script/PlayerController.cs
+++ GameJamJamJam Project/Assets/users/okada/Script/PlayerController.cs
@@ -4,21 +4,33 @@ using System.Collections;
 public class PlayerController: MonoBehaviour
 {
 
-    private float h;
-    private float v;
+    
 
-    private float speed;
-    private float jumpspeed = 12f;
+    [SerializeField]
+    private float _speed = 6;
+    public float Speed
+    {
+        get { return this._speed; }
+        set { this._speed = value; }
+    }
+
+
+    [SerializeField]
+    private float _jumpSpeed = 12;
+    public float JumpSpeed
+    {
+        get { return this._jumpSpeed; }
+        set { this._jumpSpeed = value; }
+    }
 
     private CharacterController cc;
     private Vector3 dir;
+    private float h;
 
     // Use this for initialization
     void Start()
     {
         h = 0;
-        v = 0;
-        speed = 6;
         dir = Vector3.zero;
         cc = gameObject.GetComponent<CharacterController>();
     }
@@ -32,14 +44,14 @@ public class PlayerController: MonoBehaviour
         {
             dir = new Vector3(h, 0, 0);
             dir = transform.TransformDirection(dir);
-            dir *= speed;
+            dir *= _speed;
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                dir.y = jumpspeed;
+                dir.y = _jumpSpeed;
             }
         }
-        dir.y -= 20f * Time.deltaTime;
+        dir.y -= 9f * Time.deltaTime;
         cc.Move(dir * Time.deltaTime);
 
         if (Input.GetButton("Fire1"))
