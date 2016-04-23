1c38cf816941f50b4e213fe48727fc5a55335dd4
pafuhana1213 <pafuhana@hotmail.co.jp>
2016-04-23 19:08:58 +0900

    モデルの向きを調整

--- GameJamJamJam Project/Assets/users/okada/Script/PlayerMeshController.cs
+++ GameJamJamJam Project/Assets/users/okada/Script/PlayerMeshController.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class PlayerMeshController : MonoBehaviour {
 
+    public PlayerController PlController;
+    public CharacterController CharaController;
+
+    public Animation Anim;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        PlController = this.gameObject.transform.parent.GetComponent<PlayerController>();
+        CharaController = this.gameObject.transform.parent.GetComponent<CharacterController>();
+        Anim = this.gameObject.GetComponent<Animation>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        var v = CharaController.velocity;
+
+        var look = v;
+        look.y = 0.0f;
+        look.z = -5.0f;
+        this.gameObject.transform.rotation = Quaternion.LookRotation( look );
+    }
 }
